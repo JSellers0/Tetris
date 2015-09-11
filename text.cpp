266f9ddf80a49ad3b1b6d0088a2c514da7302a4a
JSellers0 <jsellers0@gmail.com>
2015-09-11 10:05:37 -0400

    Added Copyright and License in prep for going public

--- text.cpp
+++ text.cpp
@@ -1,3 +1,23 @@
+/* 
+ * Copyright 2015 Jeffrey L. Sellers
+*/
+
+/* This file is part of Jeff's C++ tetris clone.
+
+    Jeff's C++ tetris clone is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Jeff's C++ tetris clone is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Jeff's C++ tetris clone.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
 #include "text.hpp"
 
 Text::Text()
